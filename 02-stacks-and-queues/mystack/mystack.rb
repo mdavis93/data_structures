451b58fb2817c38609e4a9c5eaf5bdcabb2c75d1
Michael Davis <mdavis93@hotmail.com>
2018-11-20 04:49:49 -0600

    Assignment Complete

--- 02-stacks-and-queues/mystack/mystack.rb
+++ 02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,20 @@ class MyStack
   end
 
   def push(item)
+    @stack[@stack.length] = item
+    self.top = item
   end
 
   def pop
+    temp = @stack[@stack.length - 1]
+    @stack.delete_at(@stack.length - 1)
+    self.top = @stack[@stack.length - 1]
+    temp
   end
 
   def empty?
+
+    # This can also be accomplished by checking if @stack.length == 0
+    @stack.empty?
   end
 end
