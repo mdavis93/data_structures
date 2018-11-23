4d78d1e1eb8e7a7ac53c831b28966e4e67ca12fd
Michael Davis <mdavis93@hotmail.com>
2018-11-23 10:56:43 -0600

    Revision: Add 'myqueue'

--- 02-stacks-and-queues/myqueue/myqueue.rb
+++ 02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,34 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue[size] = element
+    update_queue
+    @queue
   end
 
   def dequeue
+    temp = @head
+    @queue = @queue.reject { |q| q == @head }
+    update_queue
+    temp
+  end
+
+  def size
+    @queue.length
   end
 
   def empty?
+    @queue.empty?
+  end
+
+  private
+
+  def update_queue
+    @tail = @queue[size - 1]
+    @head = @queue[0]
   end
 end
